38859ef873f5a4635a213d64e94aa2a5807eaeda
angelmariel <seanhsu779@gmail.com>
2019-09-15 17:52:02 -0700

    Added activity dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,50 +1,57 @@
-import React from 'react';
-import { Header, Icon, List } from 'semantic-ui-react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
 import axios from 'axios';
 import { IActivity } from '../models/activity';
-
-interface IState {
-  activities: IActivity[];
-}
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 // {} represents executable JavaScript code
 // Using className is because class is a reserved word for JavaScript
 // Using class we will have access to lifecycle method
-class App extends React.Component<{}, IState> {
-  readonly state: IState = {
-    activities: []
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0]);
   };
 
-  // This is a good place to fetch our data. Use Axios to make http requests
-  componentDidMount() {
-    // axios.get() returns a promise (axios response), which means we can use .then() statement
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
+
+  // In order to get our data back from the API, we have to use useEffect hook
+  useEffect(() => {
+    // Use Axios to make http requests
     axios
       .get<IActivity[]>('http://localhost:5000/api/activities')
       .then(response => {
-        this.setState({
-          // data object in axios response contains what's returned from our API
-          activities: response.data
-        });
+        setActivities(response.data);
       });
-  }
+  }, []);
 
   // Component should always has render function
   // Have to use this because we are referencing a class property "state"
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map(activity => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+  // In JSX we only can return one element, but that element can contain a bunch of children
+  return (
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+        />
+      </Container>
+    </Fragment>
+  );
+};
 
 export default App;
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+
+interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void; // We need to pass in the signature of the function that we are passing down
+  selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
+}
+
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  editMode,
+  setEditMode,
+  setSelectedActivity
+}) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList activities={activities} selectActivity={selectActivity} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          />
+        )}
+        {editMode && <ActivityForm setEditMode={setEditMode} />}
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default ActivityDashboard;
